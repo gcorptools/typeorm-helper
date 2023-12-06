ec0bb38b64a514105753d4f23674082da07af45c
G-Corporationcare <gcorporationcare@gmail.com>
2023-12-06 18:13:07 +0100

    fix: Fix building issues

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from '@jest/types';
 import { pathsToModuleNameMapper } from 'ts-jest';
-import tsconfigJson from './tsconfig.json';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest/presets/default-esm',
@@ -8,12 +7,14 @@ const config: Config.InitialOptions = {
   verbose: true,
   setupFilesAfterEnv: ['./src/tests/setup.ts'],
   moduleNameMapper: pathsToModuleNameMapper(
-    tsconfigJson.compilerOptions.paths,
+    {
+      '@src/*': ['src/*']
+    },
     { prefix: '<rootDir>/' }
   ),
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
   coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/']
 };
 
 export default config;
